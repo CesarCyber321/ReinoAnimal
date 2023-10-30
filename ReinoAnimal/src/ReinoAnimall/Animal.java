4c573805361857bf1a972fd505a01ff1dfdae669
CesarFranca <cesarfranca1000@gmail.com>
2023-10-30 07:49:02 -0300

    Classe mãe do projeto, todas as outras como aquatico, terrestre e aereo, extende dela os seus atrubutos

--- ReinoAnimal/src/ReinoAnimall/Animal.java
+++ ReinoAnimal/src/ReinoAnimall/Animal.java
@@ -5,47 +5,79 @@ import java.util.Date;
 public class Animal {
 	private String especie;
 	private String sexo;
-	private Date dataNasci;
-	private double contador;
+	private String dataNasci;
+	private static int contador;
+
+	/**
+	 * @author Cesar construtor com parametro
+	 * @param especie, esse parametro é responsavel por dizer de qual especie é o animal.
+	 * @param sexo, diz o sexo do animal.
+	 * @param dataNasci, data de nascimento do animal.
+	 * @param contador
+	 */
+
+	public Animal(String especie, String sexo, String dataNasci) {
+		this.especie = especie;
+		this.sexo = sexo;
+		this.dataNasci = dataNasci;
+		this.setContador(contador + 1);
+	}
+	
+	/**
+	 * o metodo Animal é um metodo abstrato, que contem como parametros os atributos da classe.
+	 */
 
 	public void comer() {
-		System.out.println("o animal esta comendo");
+		System.out.println("o animal, "  + especie + ", esta comendo");
 	}
-
+	
+	/**
+	 * o metodo comer diz que o animal esta comendo.
+	 */
 	public void perfil() {
 		contador = contador + 1;
-		
+		System.out.println(contador);
+		System.out.println(especie);
+		System.out.println("a data de nascimento é " + dataNasci);
+		System.out.println(sexo);
 	}
 
-	public String getEspecie() {
+	/*
+	 * metodos get e set
+	 */
+
+	private String getespecie() {
 		return especie;
 	}
+	/**
+	 * @param especie
+	 */
 
-	public void setEspecie(String especie) {
+	private void setEspecie(String especie) {
 		this.especie = especie;
 	}
 
-	public String getSexo() {
+	private String getSexo() {
 		return sexo;
 	}
 
-	public void setSexo(String sexo) {
+	private void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
 
-	public Date getDataNasci() {
+	private String getDataNasci() {
 		return dataNasci;
 	}
 
-	public void setDataNasci(Date dataNasci) {
+	private void setDataNasci(String dataNasci) {
 		this.dataNasci = dataNasci;
 	}
 
-	public double getContador() {
+	private static double getContador() {
 		return contador;
 	}
 
-	private void setContador(double contador) {
+	private void setContador(int contador) {
 		this.contador = contador;
 	}
 
