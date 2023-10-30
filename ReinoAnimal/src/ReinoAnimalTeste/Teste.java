a2024a57c70f9f51c6d6465a7d88c8eab0c1071c
CesarFranca <cesarfranca1000@gmail.com>
2023-10-30 14:39:43 -0300

    Teste foi criada como a classe maen para testar as demais classes

--- ReinoAnimal/src/ReinoAnimalTeste/Teste.java
+++ ReinoAnimal/src/ReinoAnimalTeste/Teste.java
@@ -14,22 +14,41 @@ public class Teste {
 
 		// polimorfismo
 
-		/**
-		 * Aereo a = new Aereo("papagaiu", "macho", "25/10/2023", 3.2); a.comer();
-		 * a.perfil(); a.voar(); 
-		 */ 
-		  Aquatico c = new Aquatico("salgada",true ,"peixe", "femea", "22/11/2005"); 
-		  c.comer(); 
-		  c.perfil(); 
-		  c.nadar();
-		  
-		 
-		  System.out.println("------------------");
-		
-		Terrestre t = new Terrestre("mata atlantica", true ,"onça", "femea", "08/15/10");
+		Aquatico c = new Aquatico("Carcharias taurus", false, "fêmea", "7", "água salgada");
+		c.comer();
+		c.perfil();
+		c.nadar();
+
+		Aquatico C = new Aquatico("Pygocentrus nattereri", true, "macho", "3", "água doce");
+
+		System.out.println("------------------");
+
+		Terrestre t = new Terrestre("Felis catus", true, "fêmea", "2", "Ásia");
 		t.perfil();
 		t.mover();
 		t.comer();
-	
+
+		System.out.println("-------------------");
+
+		Terrestre T = new Terrestre("Hydrochoerus hydrochaeris", true, "macho", "8", "América do Sul");
+		
+		T.perfil();
+		T.mover();
+		T.comer();
+
+		System.out.println("-------------------");
+
+		Aereo a = new Aereo("Coragyps atratus", "macho", "3", 54.5);
+		a.comer();
+		a.perfil();
+		a.voar();
+		
+		System.out.println("--------------------");
+		
+		Aereo A = new Aereo("Amazona aestiva", "fêmea", "10", 37.9);
+		A.comer();
+		A.perfil();
+		A.voar();
+
 	}
 }
